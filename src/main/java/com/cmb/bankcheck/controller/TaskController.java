b9488b2254fffe38fa90439492af85257707952f
LionsChen <chenlions@163.com>
2019-07-29 14:35:48 +0800

    做任务控制器

---
+++ src/main/java/com/cmb/bankcheck/controller/TaskController.java
@@ -0,0 +1,14 @@
+package com.cmb.bankcheck.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * created by chenhanping
+ * Designer:chenhanping
+ * Date:2019-07-29
+ * Time:14:34
+ * 处理办理任务类的请求
+ */
+@RestController
+public class TaskController {
+}
