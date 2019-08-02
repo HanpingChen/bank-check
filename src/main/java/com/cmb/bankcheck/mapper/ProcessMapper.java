b23d15e7815f765396c9fba23017980cf7a8e952
zzAaron <1074949979@qq.com>
2019-08-02 10:19:26 +0800

    加了remark字段，增加了select查询

--- src/main/java/com/cmb/bankcheck/mapper/ProcessMapper.java
+++ src/main/java/com/cmb/bankcheck/mapper/ProcessMapper.java
@@ -4,6 +4,7 @@ import com.cmb.bankcheck.entity.ProcessEntity;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -31,6 +32,15 @@ public interface ProcessMapper {
      * @param processId
      * @return
      */
-    @Insert("insert into process(user_id, process_id) values (#{userId}, #{processId})")
-    int insertProcess(@Param("userId") String userId, @Param("processId") String processId);
+    @Insert("insert into process(user_id, process_id,remark) values (#{userId}, #{processId},#{remark})")
+    int insertProcess(@Param("userId") String userId, @Param("processId") String processId,@Param("status") int status,@Param("remark") String remark);
+
+    /**
+     * 根据instanceId，查询得到userId等相关信息
+     * @param processId
+     * @return
+     */
+    @Select("select * from process where processId=#{processId}")
+    public List<ProcessEntity> querytProcessByinsId(@Param("processId") String processId);
+
 }
