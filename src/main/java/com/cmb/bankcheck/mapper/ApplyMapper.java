4ceaa0be9d6a93953b0ebbcc7938a3c162c6f245
LionsChen <chenlions@163.com>
2019-08-13 09:23:48 +0800

    申请单查询接口

---
+++ src/main/java/com/cmb/bankcheck/mapper/ApplyMapper.java
@@ -0,0 +1,25 @@
+package com.cmb.bankcheck.mapper;
+
+import com.cmb.bankcheck.entity.ApplyEntity;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * created by chenhanping
+ * Designer:chenhanping
+ * Date:2019-08-09
+ * Time:16:23
+ */
+@Mapper
+public interface ApplyMapper {
+
+
+    @Select("select * from apply where apply_id = #{processId}")
+    public ApplyEntity queryApplyByProcessId(@Param("processId") String processId);
+
+    @Insert("insert into apply(apply_id, user_id, username,msg, process_key,starter,xmtype,amt,record,cor,analyse,situation, branch) " +
+            "values(#{applyId}, #{userId}, #{username}, #{msg}, #{processKey},#{starter},#{xmtype},#{amt},#{record},#{cor},#{analyse},#{situation},#{branch})")
+    public int insertApply(ApplyEntity entity);
+}
